89c46806659a2313d139fe36ad179337410b89e1
Dadasaheb kamble <156794606+dadasahebkamble171@users.noreply.github.com>
2024-01-16 21:27:13 +0530

    Create Day1.sql

---
+++ Day1.sql
@@ -0,0 +1,4 @@
+-- enter your SQL query here
+-- use the SQL editor UI in browser to browse the SQL database
+-- only write your SELECT statement here, comment rest of the statements if present
+SELECT first_name,last_name FROM 'patients' where weight between 100 and 120;
