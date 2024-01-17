203111a8a010c9996c2bef7302c1613694edaf2e
Dadasaheb kamble <156794606+dadasahebkamble171@users.noreply.github.com>
2024-01-17 19:24:26 +0530

    Day2.sql

--- Day2.sql
+++ Day2.sql
@@ -1,1 +1,4 @@
+-- enter your SQL query here
+-- use the SQL editor UI in browser to browse the SQL database
+-- only write your SELECT statement here, comment rest of the statements if present
 select first_name,last_name,allergies from patients where city='Hamilton' and allergies not null;
