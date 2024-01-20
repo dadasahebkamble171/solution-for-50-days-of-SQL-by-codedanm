c688b2e93b2baefc23d4e313346820f2983325d8
Dadasaheb kamble <156794606+dadasahebkamble171@users.noreply.github.com>
2024-01-20 19:17:32 +0530

    Day5.sql

---
+++ Day5.sql
@@ -0,0 +1,7 @@
+-- enter your SQL query here
+-- use the SQL editor UI in browser to browse the SQL database
+-- only write your SELECT statement here, comment rest of the statements if present
+
+
+
+SELECT * FROM 'patients' where patient_id in (1,45,534,879,1000);
