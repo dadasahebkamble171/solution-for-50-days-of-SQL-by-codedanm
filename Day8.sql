b172e117ea4e2c35c98076b10143721a1bda8b13
Dadasaheb kamble <156794606+dadasahebkamble171@users.noreply.github.com>
2024-01-23 19:01:37 +0530

    Day8.sql

---
+++ Day8.sql
@@ -0,0 +1,6 @@
+-- enter your SQL query here
+-- use the SQL editor UI in browser to browse the SQL database
+-- only write your SELECT statement here, comment rest of the statements if present
+
+
+SELECT * FROM 'admissions' where admission_date = discharge_date;
