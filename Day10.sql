3e5928fafd68836f2261fc6d9e6b532f49a31b76
Dadasaheb kamble <156794606+dadasahebkamble171@users.noreply.github.com>
2024-01-25 00:14:26 +0530

    Day10.sql

--- Day10.sql
+++ Day10.sql
@@ -1,4 +1,9 @@
 -- enter your SQL query here
 -- use the SQL editor UI in browser to browse the SQL database
 
-SELECT order_id FROM  'orders' where MOD(order _id,2)=0;
+SELECT 
+  order_id
+  FROM 
+  'orders' 
+  where
+  MOD(order _id,2)=0;
